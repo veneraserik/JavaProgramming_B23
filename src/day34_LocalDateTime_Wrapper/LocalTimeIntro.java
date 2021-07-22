7d13d28e2ebf48b5bd21e0bce83b0f84e28ada4d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-22 11:41:14 -0400

    day34

---
+++ src/day34_LocalDateTime_Wrapper/LocalTimeIntro.java
@@ -0,0 +1,28 @@
+package day34_LocalDateTime_Wrapper;
+
+import java.time.LocalTime;
+
+public class LocalTimeIntro {
+
+    public static void main(String[] args) {
+
+        LocalTime  now = LocalTime.now();
+
+        System.out.println(now);
+
+        LocalTime breakTime = now.plusMinutes(15);
+
+        System.out.println(breakTime);
+
+        LocalTime quizDue = breakTime.plusMinutes(20);
+
+        System.out.println(quizDue);
+
+
+        LocalTime comeBackAt = LocalTime.of(11, 40);
+
+        System.out.println("COme Back at: "+ comeBackAt);
+
+    }
+
+}
