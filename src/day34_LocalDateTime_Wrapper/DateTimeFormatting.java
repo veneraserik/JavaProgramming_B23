d83dc1c2fb7d9e899513cc3411732309dce89033
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-22 12:39:24 -0400

    day34

--- src/day34_LocalDateTime_Wrapper/DateTimeFormatting.java
+++ src/day34_LocalDateTime_Wrapper/DateTimeFormatting.java
@@ -38,10 +38,28 @@ public class DateTimeFormatting {
         System.out.println(a);
         System.out.println( a.format(DTF) );
 
+        System.out.println("---------------------------------------------------");
+        //Tuesday, 1:00 pm, Nov/24/2020
+         DateTimeFormatter format = DateTimeFormatter.ofPattern("EEEE, hh:mm a, MMM/dd/YYYY");
 
+        LocalDateTime time2 = LocalDateTime.of(2021, 7, 23, 13, 0);
+        System.out.println(time2);
 
+        System.out.println(time2.format(format));
+
+        // what day is 2022-01-01
+       String result =  LocalDate.of(2022,2,28).format(  DateTimeFormatter.ofPattern("EEEE")  );
+        System.out.println("result = " + result);
+
+        System.out.println(nameOfTheDay(2025, 5, 28));
 
     }
+
+    public static String nameOfTheDay(int year, int month, int day){
+        String result =  LocalDate.of(year, month,day).format(  DateTimeFormatter.ofPattern("EEEE")  );
+        return result;
+    }
+
 
 
 }
