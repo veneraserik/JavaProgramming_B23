266ac984f396eaaed3449849442fe3f5ce6cb5cc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-23 11:22:11 -0400

    day35

---
+++ src/day35_ArrayList/ArrayListIntro.java
@@ -0,0 +1,21 @@
+package day35_ArrayList;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListIntro {
+
+    public static void main(String[] args) {
+        int[] nums = new int[10]; // size is 10 fixed
+        System.out.println(Arrays.toString(nums));
+
+        ArrayList<Integer> numbers = new ArrayList<>(); // size is 0
+
+        System.out.println(numbers);
+
+
+    }
+
+
+}
