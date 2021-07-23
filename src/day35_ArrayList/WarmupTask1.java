6124681b6e0a6e486d8e3847af0d890974f8de1a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-23 10:34:26 -0400

    day35

---
+++ src/day35_ArrayList/WarmupTask1.java
@@ -0,0 +1,18 @@
+package day35_ArrayList;
+
+public class WarmupTask1 {
+
+    
+
+}
+
+
+/*
+
+1. create an array of LocalDate and has the next 10 day's dates, use for each loop to print each Date in the following formar:
+                MonthName/Day, Day Name
+
+                ex:
+                    November/25, Wednesday
+
+ */
